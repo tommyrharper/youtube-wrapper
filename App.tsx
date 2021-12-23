6a477f1ac1c203474a45e9473390a94b8db4b487
tommyrharper <thomasrobertharper@gmail.com>
2021-12-23 23:10:21 +0000

    Update time picker values to numbers

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, View, Modal } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
 export default function App() {
-  const [lengthOfTime, setLengthOfTime] = useState();
+  const [lengthOfTime, setLengthOfTime] = useState(5);
   return (
     <View style={styles.container}>
       <Text>How long would you like to watch for?</Text>
@@ -12,12 +12,12 @@ export default function App() {
         selectedValue={lengthOfTime}
         onValueChange={setLengthOfTime}
       >
-        <Picker.Item label="5 mins" value="java" />
-        <Picker.Item label="10 mins" value="js" />
-        <Picker.Item label="15 mins" value="js" />
-        <Picker.Item label="20 mins" value="js" />
-        <Picker.Item label="25 mins" value="js" />
-        <Picker.Item label="30 mins" value="js" />
+        <Picker.Item label="5 mins" value={5} />
+        <Picker.Item label="10 mins" value={10} />
+        <Picker.Item label="15 mins" value={15} />
+        <Picker.Item label="20 mins" value={20} />
+        <Picker.Item label="25 mins" value={25} />
+        <Picker.Item label="30 mins" value={30} />
       </Picker>
     </View>
   );
