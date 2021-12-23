851f595fe40c28f07f0b49af0eae96c237e6dceb
tommyrharper <thomasrobertharper@gmail.com>
2021-12-23 22:09:23 +0000

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("example");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>How long would you like to watch for?</Text>
+      <TextInput style={styles.textInput} onChangeText={setText} value={text} />
     </View>
   );
 }
@@ -11,8 +14,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 30,
+    padding: 10,
+    width: 200,
   },
 });
