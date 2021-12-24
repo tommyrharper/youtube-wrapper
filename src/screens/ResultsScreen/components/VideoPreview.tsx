276d8bb8efe995bc219553238c47c42880b466a4
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 18:36:05 +0000

    Add image to VideoPreview.tsx

--- src/screens/ResultsScreen/components/VideoPreview.tsx
+++ src/screens/ResultsScreen/components/VideoPreview.tsx
@@ -1,15 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { VideoItem } from '../../../types';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { VideoSearchItem } from '../../../types';
 
 interface VideoPreviewProps {
-  video: VideoItem;
+  video: VideoSearchItem;
 }
 
 export const VideoPreview = ({ video }: VideoPreviewProps) => {
   return (
-    <View>
-      <Text>{video.snippet.title}</Text>
+    <View style={styles.container}>
+      <Text>
+        {video.snippet.title}
+      </Text>
+      <Text>
+        {video.snippet.channelTitle}
+      </Text>
+      <Image
+        style={styles.image}
+        source={{ uri: video.snippet.thumbnails.default.url }}
+      />
+      {/* <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: 'https://reactnative.dev/img/tiny_logo.png',
+        }}
+      /> */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 300,
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 300,
+    height: 200,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+});
