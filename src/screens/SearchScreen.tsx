7a1c42476dc5b12182ae1b47153c938aa33d5e63
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 10:46:10 +0000

    Add searchTerm to SearchScreen

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
-import Constants from 'expo-constants';
 import { CTAButton } from '../components/CTAButton';
 import { Heading } from '../components/Heading';
 import { DefaultContainer } from '../components/DefaultContainer';
 import { Input } from '../components/Input';
+import { useStore } from '../zustandStore';
 
 export const SearchScreen = () => {
   const { navigate, goBack } = useNavigation();
-
-  console.log(`Constants`, Constants.manifest.extra.googleApiKey)
+  const { searchTerm, setSearchTerm } = useStore();
 
   return (
     <DefaultContainer>
       <Heading>Search</Heading>
-      <Input />
-      <CTAButton onPress={() => navigate('Search')}>Go</CTAButton>
+      <Input value={searchTerm} onChangeText={setSearchTerm} />
+      <CTAButton onPress={() => navigate('Results')}>Go</CTAButton>
       <CTAButton onPress={goBack}>Back</CTAButton>
     </DefaultContainer>
   );
