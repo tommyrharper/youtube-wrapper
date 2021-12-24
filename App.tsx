0225aff014ce89806769e7c2b030682b4ca8897e
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 00:03:42 +0000

    Add navigation container  to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
-  const [lengthOfTime, setLengthOfTime] = useState(5);
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
+  const [lengthOfTime, setLengthOfTime] = useState<number>(5);
   return (
     <View style={styles.container}>
       <Text>How long would you like to watch for?</Text>
@@ -20,6 +24,20 @@ export default function App() {
         <Picker.Item label="30 mins" value={30} />
       </Picker>
     </View>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
