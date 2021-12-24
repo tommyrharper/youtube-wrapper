5cb9e5b13cf0f757357b644d411fc892644a1d82
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 11:46:54 +0000

    Create DefaultQueryContainer

---
+++ src/components/DefaultQueryContainer.tsx
@@ -0,0 +1,37 @@
+import { useNavigation } from '@react-navigation/core';
+import React, { ReactNode } from 'react';
+import { UseFetchResult } from '../hooks/useFetch';
+import { CTAButton } from './CTAButton';
+import { DefaultContainer } from './DefaultContainer';
+import { ErrorMessage } from './ErrorMessage';
+import { Heading } from './Heading';
+
+interface DefaultQueryContainerProps {
+  children: ReactNode;
+  query: UseFetchResult;
+}
+
+export const DefaultQueryContainer = ({
+  children,
+  query,
+}: DefaultQueryContainerProps) => {
+  const { goBack } = useNavigation();
+  const { loading, error } = query;
+  return (
+    <DefaultContainer>
+      {loading && (
+        <>
+          <Heading>Loading...</Heading>
+          <CTAButton onPress={goBack}>Back</CTAButton>
+        </>
+      )}
+      {error && !loading && (
+        <>
+          <ErrorMessage>There was an Error</ErrorMessage>
+          <CTAButton onPress={goBack}>Back</CTAButton>
+        </>
+      )}
+      {!error && !loading && children}
+    </DefaultContainer>
+  );
+};
