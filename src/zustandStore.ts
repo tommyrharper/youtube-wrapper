ba4353b01f6bbe39264e5aee2868f59ef34e5d8d
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 10:44:41 +0000

    Add searchTerm to zustandStore

--- src/zustandStore.ts
+++ src/zustandStore.ts
@@ -3,14 +3,17 @@ import create from 'zustand';
 interface ZustandStore {
   lengthOfTime: number;
   noOfVideos: number;
+  searchTerm: string;
   setLengthOfTime: (lengthOfTime: number | string) => void;
   setNoOfVideos: (noOfVideos: number | string) => void;
+  setSearchTerm: (searchTerm: string) => void;
 }
 
 export const useStore = create<ZustandStore>((set) => ({
   // initial state
   lengthOfTime: 0,
   noOfVideos: 0,
+  searchTerm: '',
   // methods for manipulating state
   setLengthOfTime: (lengthOfTime: number | string) => {
     set(() => ({
@@ -20,6 +23,11 @@ export const useStore = create<ZustandStore>((set) => ({
   setNoOfVideos: (noOfVideos: number | string) => {
     set(() => ({
       noOfVideos: Number(noOfVideos),
+    }));
+  },
+  setSearchTerm: (searchTerm: string) => {
+    set(() => ({
+      searchTerm,
     }));
   },
 }));
