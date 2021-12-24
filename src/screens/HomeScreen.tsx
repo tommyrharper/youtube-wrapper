4eb089c492e0f4b73022f1b0b356ea500291097a
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 00:22:37 +0000

    Create HomeScreen.tsx

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { CTAButton } from "../components/CTAButton";
+
+export const HomeScreen = () => {
+  const [lengthOfTime, setLengthOfTime] = useState<number>(5);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.question}>How long would you like to watch for?</Text>
+      <Picker
+        style={styles.picker}
+        selectedValue={lengthOfTime}
+        onValueChange={setLengthOfTime}
+      >
+        <Picker.Item label="5 mins" value={5} />
+        <Picker.Item label="10 mins" value={10} />
+        <Picker.Item label="15 mins" value={15} />
+        <Picker.Item label="20 mins" value={20} />
+        <Picker.Item label="25 mins" value={25} />
+        <Picker.Item label="30 mins" value={30} />
+      </Picker>
+      <CTAButton onPress={() => {}}>Start</CTAButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  question: {
+    fontSize: 40,
+    textAlign: "center",
+    padding: 30,
+  },
+  picker: {
+    width: "40%",
+  },
+});
