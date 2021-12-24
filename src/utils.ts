74608fe392fe8f2b7eeccc623162b2e6a17beb98
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 19:52:57 +0000

    Rename reptms to regex

--- src/utils.ts
+++ src/utils.ts
@@ -62,14 +62,14 @@ export const getTransformedVideoResponse = (
 };
 
 export const convertISO8601ToSeconds = (input) => {
-  const reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+  const regex = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
   let hours = 0;
   let minutes = 0;
   let seconds = 0;
   let totalSeconds;
 
-  if (reptms.test(input)) {
-    const matches = reptms.exec(input);
+  if (regex.test(input)) {
+    const matches = regex.exec(input);
     if (matches[1]) hours = Number(matches[1]);
     if (matches[2]) minutes = Number(matches[2]);
     if (matches[3]) seconds = Number(matches[3]);
