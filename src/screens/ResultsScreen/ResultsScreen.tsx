31c2b7b8f3e765ea12c94ef1f2eeb4fcbde105a0
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 19:10:24 +0000

    Use useFetchResults in ResultsScreen

--- src/screens/ResultsScreen/ResultsScreen.tsx
+++ src/screens/ResultsScreen/ResultsScreen.tsx
@@ -3,18 +3,13 @@ import { useNavigation } from '@react-navigation/native';
 import { FlatList } from 'react-native';
 import { CTAButton } from '../../components/CTAButton';
 import { Heading } from '../../components/Heading';
-import { mockResults } from '../../mockResults';
 import { DefaultQueryContainer } from '../../components/DefaultQueryContainer';
 import { VideoPreview } from './components/VideoPreview';
+import { useFetchResults } from './hooks/useFetchResults';
 
 export const ResultsScreen = () => {
-  const { navigate, goBack } = useNavigation();
-  // const query = useFetchResults()
-  const query = {
-    loading: false,
-    error: false,
-    data: mockResults,
-  };
+  const { goBack } = useNavigation();
+  const query = useFetchResults();
   const { data } = query;
 
   const renderItem = useCallback(({ item }) => {
@@ -26,7 +21,7 @@ export const ResultsScreen = () => {
       <Heading>Results</Heading>
       <FlatList
         data={data.items}
-        keyExtractor={(item) => item.id.videoId}
+        keyExtractor={(item) => item.etag}
         renderItem={renderItem}
       />
       <CTAButton onPress={goBack}>Back</CTAButton>
