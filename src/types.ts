deba8d3e2a320057b71dcfb4215915e4218a42f7
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 19:09:24 +0000

    Update types with VideoListItem

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export interface VideoItem {
+export interface VideoSearchItem {
   kind: string;
   etag: string;
   id: {
@@ -31,4 +31,54 @@ export interface VideoItem {
     liveBroadcastContent: string;
     publishTime: string;
   };
+}
+
+export interface YouTubeSearchResponse {
+  kind: 'youtube#searchListResponse';
+  etag: string;
+  nextPageToken: string;
+  regionCode: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: VideoSearchItem[];
+}
+
+export interface VideoListItem {
+  kind: string;
+  etag: string;
+  id: string;
+  contentDetails: {
+    duration: string;
+    dimension: string;
+    definition: string;
+    caption: string;
+    licensedContent: boolean;
+    contentRating: object;
+    projection: string;
+  };
+  statistics: {
+    viewCount: string;
+    likeCount: string;
+    favoriteCount: string;
+    commentCount: string;
+  };
+}
+
+export interface VideoListResponse {
+  kind: 'youtube#videoListResponse';
+  etag: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: VideoListItem[];
+}
+
+// TODO: Check this is correct
+export type TransformedVideoItem = VideoListItem & VideoSearchItem;
+// TODO: Check these is correct
+export interface TransformedVideosResponse extends YouTubeSearchResponse {
+  items: TransformedVideoItem[];
 }
