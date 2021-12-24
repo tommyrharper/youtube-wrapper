b95e6728f22cfcc9ceed78f4171903f643d706ce
tommyrharper <thomasrobertharper@gmail.com>
2021-12-24 00:48:30 +0000

    Create RootStackParamList

--- src/NavigationStack.tsx
+++ src/NavigationStack.tsx
@@ -2,8 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import { HomeScreen } from "./screens/HomeScreen";
+import { NumberVideosScreen } from "./screens/NumberVideosScreen";
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  NumberVideos: {
+    lengthOfTime: number;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const NavigationStack = () => {
   return (
@@ -12,7 +20,12 @@ export const NavigationStack = () => {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          // options={{ title: "Welcome" }}
+          options={{ title: "Welcome", headerShown: false }}
+        />
+        <Stack.Screen
+          name="NumberVideos"
+          component={NumberVideosScreen}
+          options={{ title: "", headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
