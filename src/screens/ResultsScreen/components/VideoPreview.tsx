d07ccbc83ef3c09e0efdde48de7442c7bf417748
tommyrharper <thomasrobertharper@gmail.com>
2021-12-25 00:26:40 +0000

    Add navigation from VideoPreview to VideoScreen

--- src/screens/ResultsScreen/components/VideoPreview.tsx
+++ src/screens/ResultsScreen/components/VideoPreview.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import React, { useCallback } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { TransformedVideoItem } from '../../../types';
 import { abbreviateNumber, getVideoDurationString } from '../../../utils';
 
@@ -8,21 +9,29 @@ interface VideoPreviewProps {
 }
 
 export const VideoPreview = ({ video }: VideoPreviewProps) => {
+  const { navigate } = useNavigation();
+  const onPress = useCallback(
+    () => navigate('Video', { video }),
+    [navigate, video],
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{video.snippet.title}</Text>
-      <Text style={styles.text}>- {video.snippet.channelTitle}</Text>
-      <Text style={styles.text}>
-        Length: {getVideoDurationString(video.contentDetails.duration)}
-      </Text>
-      <Text style={styles.text}>
-        Views: {abbreviateNumber(video.statistics.viewCount)}
-      </Text>
-      <Image
-        style={styles.image}
-        source={{ uri: video.snippet.thumbnails.default.url }}
-      />
-    </View>
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <Text style={styles.text}>{video.snippet.title}</Text>
+        <Text style={styles.text}>- {video.snippet.channelTitle}</Text>
+        <Text style={styles.text}>
+          Length: {getVideoDurationString(video.contentDetails.duration)}
+        </Text>
+        <Text style={styles.text}>
+          Views: {abbreviateNumber(video.statistics.viewCount)}
+        </Text>
+        <Image
+          style={styles.image}
+          source={{ uri: video.snippet.thumbnails.default.url }}
+        />
+      </View>
+    </TouchableOpacity>
   );
 };
 
