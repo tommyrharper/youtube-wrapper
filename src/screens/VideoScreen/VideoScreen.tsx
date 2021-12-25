ccefc0e202950f87f81d69dc8db81c3bbdde127d
tommyrharper <thomasrobertharper@gmail.com>
2021-12-25 00:46:17 +0000

    Add YouTube player to VideoScreen

--- src/screens/VideoScreen/VideoScreen.tsx
+++ src/screens/VideoScreen/VideoScreen.tsx
@@ -1,8 +1,9 @@
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/core';
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { Text, StyleSheet, Alert } from 'react-native';
+import YoutubePlayer from 'react-native-youtube-iframe';
 import { CTAButton } from '../../components/CTAButton';
-import { DefaultQueryContainer } from '../../components/DefaultQueryContainer';
+import { DefaultContainer } from '../../components/DefaultContainer';
 import { RootStackParamList } from '../../NavigationStack';
 import { getVideoDurationString, abbreviateNumber } from '../../utils';
 
@@ -10,24 +11,41 @@ export const VideoScreen = () => {
   const { params } = useRoute<RouteProp<RootStackParamList, 'Video'>>();
   const { video } = params;
   const { goBack } = useNavigation();
-  const query = {
-    data: {},
-    loading: false,
-    error: false,
-    errorMessage: undefined,
-  };
+
+  const [playing, setPlaying] = useState(false);
+
+  const onStateChange = useCallback((state) => {
+    if (state === 'ended') {
+      setPlaying(false);
+      Alert.alert('video has finished playing!');
+    }
+  }, []);
+
+  const togglePlaying = useCallback(() => {
+    setPlaying((prev) => !prev);
+  }, []);
+
   return (
-    <DefaultQueryContainer query={query}>
+    <DefaultContainer>
       <Text style={styles.text}>{video.snippet.title}</Text>
       <Text style={styles.text}>- {video.snippet.channelTitle}</Text>
+      <YoutubePlayer
+        height={300}
+        play={playing}
+        videoId={video.id.videoId}
+        onChangeState={onStateChange}
+      />
       <Text style={styles.text}>
         Length: {getVideoDurationString(video.contentDetails.duration)}
       </Text>
       <Text style={styles.text}>
         Views: {abbreviateNumber(video.statistics.viewCount)}
       </Text>
+      <CTAButton onPress={togglePlaying}>
+        {playing ? 'pause' : 'play'}
+      </CTAButton>
       <CTAButton onPress={goBack}>Back</CTAButton>
-    </DefaultQueryContainer>
+    </DefaultContainer>
   );
 };
 
